b98b37983a48c0e71c179d0d3bac48741b4c7e3c
Spimods <132432388+Spimods@users.noreply.github.com>
2024-05-14 19:27:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def timer(end_date):
         days = time_remaining.days
         hours, remainder = divmod(time_remaining.seconds, 3600)
         minutes, _ = divmod(remainder, 60)
-        return "{} jours, {} heures, {} minutes".format(days, hours, minutes)
+        return "{}j {}h {}min".format(days, hours, minutes)
 
 async def generator_img(player_data_1, player_data_2, player_data_3):
     image_width = 1400
